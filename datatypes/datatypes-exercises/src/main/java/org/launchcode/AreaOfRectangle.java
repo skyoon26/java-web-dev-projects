54b185b7d44a51f6a806197ff3e0f0b1ec1d76b1
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-04-01 14:50:18 -0500

    Create a new class to utilize user input in order to calculate the area of a rectangle

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaOfRectangle.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaOfRectangle.java
@@ -1,2 +1,19 @@
-package org.launchcode;public class AreaOfRectangle {
+package org.launchcode;
+
+import java.util.Scanner;
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the length of the rectangle: ");
+        int length = input.nextInt();
+
+        System.out.println("Enter the width of the rectangle: ");
+        int width = input.nextInt();
+
+        input.close();
+
+        int area = length * width;
+        System.out.println(area);
+    }
 }
